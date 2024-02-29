ae48a6bcf6bb815e23ea391c0354ce1893f72158
varia <ltnhan2610@gmail.com>
2024-02-29 11:04:33 +0700

    remove authentication

--- backend/src/api/routes/v1/index.js
+++ backend/src/api/routes/v1/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userRoutes = require('./user.route');
-const authRoutes = require('./auth.route');
 
 const router = express.Router();
 
@@ -15,6 +14,5 @@ router.get('/status', (req, res) => res.send('OK'));
 router.use('/docs', express.static('docs'));
 
 router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
 
 module.exports = router;
