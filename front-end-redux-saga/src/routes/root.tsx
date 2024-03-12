a5a043e6acd8055b7af5c130d01756ba0898faea
varia <ltnhan2610@gmail.com>
2024-03-12 08:26:38 +0700

    use react router dom Link in navbar

--- front-end-redux-saga/src/routes/root.tsx
+++ front-end-redux-saga/src/routes/root.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -22,13 +22,27 @@ export default function Root() {
 		<>
 			<Navbar expand="lg" className="bg-body-tertiary">
 				<Container>
-					<Navbar.Brand href="/">ZigvyBlog</Navbar.Brand>
+					<Navbar.Brand>
+						<Link className="navbar-brand" to="/">ZigvyBlog</Link>{" "}
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
-							<Nav.Link href="/">Home</Nav.Link>
-							<Nav.Link href="/">About</Nav.Link>
-							<Nav.Link href="/">Contact</Nav.Link>
+							<Nav.Link>
+								<Link className="nav-link" to="/">
+									Home
+								</Link>{" "}
+							</Nav.Link>
+							<Nav.Link>
+								<Link className="nav-link" to="/posts/1">
+									About
+								</Link>{" "}
+							</Nav.Link>
+							<Nav.Link>
+								<Link className="nav-link" to="/search">
+									Contact
+								</Link>{" "}
+							</Nav.Link>
 						</Nav>
 						<Form>
 							<Row>
